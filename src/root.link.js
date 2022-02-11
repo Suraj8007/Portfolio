f472f1c84fbaf9601ded39839a6706d55eb877c7
Suraj Ughade <surajughade5@gmail.com>
2022-02-11 17:37:57 +0530

    deleted readme

--- src/root.link.js
+++ src/root.link.js
@@ -5,7 +5,7 @@ export const FIRST_NAME = 'Suraj';
 export const LAST_NAME = 'Ughade';
 export const ADDRESS = 'Yavatmal, Maharashtra'
 export const EMAIL_ID = 'surajughade5@gmail.com@gmail.com';
-export const TWITTER_ID = 'Vk_javiya';
+export const TWITTER_ID = 'surajughade4@';
 export const FRONT_PAGE_NAME = 'Suraj Ughade';
 export const FRONT_PAGE_DESC = 'A Full-Stack Web Developer.I like to build things for web and solving problems.'
 
